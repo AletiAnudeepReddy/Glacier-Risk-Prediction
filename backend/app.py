8b8871464f42afe7746dbac64e79438adb78a323
Aleti Anudeep Reddy <aletianudeepreddy8@gmail.com>
2026-04-16 23:19:30 +0530

    add

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from backend.routes.timeline import router as timeline_router
 from backend.routes.confusion import router as confusion_router
 from backend.routes.features import router as features_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Register routes
 app.include_router(timeline_router)
